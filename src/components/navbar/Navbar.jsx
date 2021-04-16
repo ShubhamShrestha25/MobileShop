c9e5b61fb59dc16948a7b3b19608f7d86c885c3f
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-04-16 22:23:44 +0545

    Adding navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { MenuItems } from './MenuItems'
+import './Navbar.css'
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+
+const Navbar = () => {
+    const [clicked, setclicked] = useState(false)
+    const clickHandler = () =>{
+        setclicked (!clicked);
+    }
+    return (
+        <nav className="navbarItems">
+            <h1 className="navbar-logo">
+                MobileS
+            </h1>
+            <div className="menu-icon" onClick={clickHandler}>
+               {clicked ?  <CloseIcon />  :  <MenuIcon/>}
+            </div>
+            <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+                {MenuItems.map((item, index) => {
+                    return(
+                        <li key={index}>
+                        <a className={item.cName} href={item.url}>{item.title}</a>
+                        </li>
+                    )                                    
+                })}
+            </ul>
+            <div className="rightIcons">
+            <ShoppingCartIcon />
+            <AccountCircleIcon />
+            </div>   
+        </nav>
+    )
+    
+}
+
+export default Navbar
+  
