5facc56f44c323ed81511bd37655f466bbcdad8d
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-04-17 14:52:43 +0545

    Adding Image Slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import { SliderData } from "./components/Hero/SliderData";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero slides={SliderData} />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './Hero.css'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+const Hero = ({slides}) => {
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+    const timeout = useRef(null)
+
+    useEffect(() => {
+       const nextSlide = () => {
+           setCurrent(current => (current === length - 1 ? 0 : current + 1))
+       }
+       timeout.current = setTimeout(nextSlide,5000)
+       return function () {
+           if(timeout.current){
+               clearTimeout(timeout.current)
+           }
+       }
+    }, [current, length])
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current +1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current -1)
+    }
+
+    return (
+        <div className="heroSection">
+            <div className="heroWrapper">
+                {slides.map((slide, index) => {
+                    return (
+                        <div className="heroSlide" key={index}> 
+                        {index === current && (
+                        <div className="heroSlider">
+                        <img className="heroImage" src={slide.image} alt={slide.alt}/>
+                        <div className="heroContent">
+                            <h1>{slide.title}</h1>
+                            <p>{slide.price}</p>
+                        </div>
+                        </div>
+                        )}
+                        </div>
+                    )
+                })}
+                <div className="sliderButtons">
+                <ArrowBackIcon className="arrow" onClick={prevSlide}/>
+                <ArrowForwardIcon className="arrow" onClick={nextSlide} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
