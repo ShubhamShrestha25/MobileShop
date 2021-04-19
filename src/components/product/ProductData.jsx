c8240e35b5ebd78919ebf63cf7c6d3adcb4fd987
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-04-19 08:12:40 +0545

    images updates

--- src/components/product/ProductData.jsx
+++ src/components/product/ProductData.jsx
@@ -1,86 +1,99 @@
+import i1 from '../images/Products/i1.jpg'
+import i2 from '../images/Products/i2.jpg'
+import i3 from '../images/Products/i3.jpg'
+import i4 from '../images/Products/i4.jpg'
+import i5 from '../images/Products/i5.jpg'
+import i6 from '../images/Products/i6.jpg'
+import i7 from '../images/Products/i7.jpg'
+import i8 from '../images/Products/i8.jpg'
+import i9 from '../images/Products/i9.jpg'
+import i10 from '../images/Products/i10.jpg'
+import i11 from '../images/Products/i11.jpg'
+import i12 from '../images/Products/i12.jpg'
+
 export const ProductData = [
     {
     id: 1,
     title: "Samsung galaxy s21+",
     price: 'Rs 129,999',
     description: "The Samsung Galaxy S21+ is the bigger sibling of the regular Galaxy S21. Specification wise the phone carries a Snapdragon 888 chipset, 5G capability, 128/256GB storage, and a 4800mAh battery. The 6.7-inch Dynamic AMOLED display features an adaptive 120Hz refresh rate. There is a triple-camera setup with a 12MP main sensor on the back. ",
-    image: "",
+    image: i1,
     },
     {
     id: 2,
     title: "Xiaomi Mi 11 Ultra",
     price: 'Rs 107,157',
     description: "The most obvious design feature of the Xiaomi Mi 11 Ultra is the humongous camera bump on the back of the phone. Placed next to the triple camera system (more on this later) there’s a 1.1-inch 126 x 294 color AMOLED display. It has an always-on feature, it can display notifications, and it also acts like a mirror when you take a selfie using the main camera system. It’s a really interesting solution and we’re itching to try it out in a real-life scenario. The tiny display is also quite bright with a peak brightness of 450 nits. Speaking of displays, the Mi 11 Ultra comes equipped with a top-of-the-line 120Hz 6.81-inch main AMOLED panel. It’s a curved display with a resolution of 3200 x 1440 pixels and a 20:9 aspect ratio. Xiaomi cites 900 nits typical brightness and up to 1,700 nits peak brightness, and there’s also HDR10+ and Dolby Vision baked into the panel. Fast refresh-rate displays are now becoming a standard, and the Mi 11 Ultra delivers on this front as well. It features a 120Hz refresh rate display and thanks to the AdaptiveSync technology, the panel can switch between 30/60/90 and 120Hz depending on the situation. This is accompanied by a 480Hz touch sampling rate - one of the highest in any smartphone to date.",
-    image: "",
+    image: i2,
     },
     {
     id: 3,
     title: "Iphone 12 pro max",
     price: 'Rs 185,000',
     description: "",
-    image: "",
+    image: i3,
     },
     {
     id: 4,
     title: "Samsung Galaxy S21 Ultra",
     price: 'Rs 151,999',
     description: "The Samsung Galaxy S21 Ultra is the headliner of the S21 series. It's the first S series phone to support Samsung's S Pen. Specifications are top-notch including Snapdragon 888 chipset, 5G capability, up to 16gigs of RAM, and 512GB of storage. A 5000mAh battery keeps the lights on for this powerhouse. In the camera department, a quad-camera setup is presented with two telephoto sensors.",
-    image: "",
+    image: i4,
     },
     {
     id: 5,
     title: "OnePlus 9 Pro",
     price: 'Rs 112,999',
     description: "the OnePlus 9 Pro is easily one of the best-specced Android phones announced so far in 2021. With the new Snapdragon 888 on board, the OnePlus 9 Pro is a beast in terms of processing, seemingly always in excess of processing power. You can have the phone with either 8 or 12GB of RAM, and 128 or 256GB of native storage without any expansion options, which is a pretty standard and mostly sufficient loadout for Android flagships these days. There is a 6.7-inch AMOLED display on board and a a triple camera that utilizes a new 50MP ultra-wide, wide-angle, and telephoto cameras.",
-    image: "",
+    image: i5,
     },
     {
     id: 6,
     title: "Iphone 12",
     price: 'Rs 141,999',
     description: "Apple iPhone 12 is the bigger version of the regular iPhone 12 mini. The handset's hardware include a 6.1-inch OLED display, 5nm Apple A14 Bionic processor, and a dual-camera setup with a large sensor. 5G and Face ID are on board, too.",
-    image: "",
+    image: i6,
     },
     {
     id: 7,
     title: "OnePlus Nord",
     price: 'Rs 53,999',
     description: "The OnePlus Nord specifications include a 6.44-inch Fluid AMOLED display with 90Hz refresh rate, Snapdragon 765G chipset with 8/12GB RAM, and 128/256 gigs of storage. There is a quad-camera setup on the back, while the front is occupied by a dual-sensor selfie shooter. The phone is powered by 4115mAh battery and Android 10, while 5G network connectivity is offered as standard.",
-    image: "",
+    image: i7,
     },
     {
     id: 8,
     title: "Xiaomi Poco F3",
     price: 'Rs 52,000',
     description: "The Xiaomi Poco F3 specs include a 6.67-inch display with a 120Hz refresh rate, Snapdragon 870 processor, 6/8GB RAM, and 128/256GB storage. The phone is powered by a 4520mAh battery and Android 11. There is a triple camera setup on the back, as well as a 20MP front shooter. ",
-    image: "",
+    image: i8,
     },
     {
     id: 9,
     title: "Xiaomi Note 10 Pro",
     price: 'Rs 28,999',
     description: "The phone comes with a 6.67-inch touchscreen display. Redmi Note 10 Pro is powered by an octa-core Qualcomm Snapdragon 732G processor. It comes with 6GB of RAM. The Redmi Note 10 Pro runs Android 11 and is powered by a 5050mAh battery.",
-    image: "",
+    image: i9,
     },
     {
     id: 10,
     title: "Xiaomi Poco X3",
     price: 'Rs 29,999',
     description: "Xiaomi Poco X3 NFC specs include a 6.7-inch display, Snapdragon 732G processor with 6GB RAM and 64/128GB storage, and a 5160 mAh battery. There is a quad-camera setup on the back of the phone.",
-    image: "",
+    image: i10,
     },
     {
     id: 11,
     title: "Oppo A15",
     price: 'Rs 18,000',
     description: "The phone comes with a 6.52-inch display with a resolution of 720x1600 pixels and an aspect ratio of 20:9. Oppo A15 is powered by an octa-core MediaTek Helio P35 (MT6765) processor. It comes with 3GB of RAM. The Oppo A15 runs Android 10 and is powered by a 4230mAh battery.",
-    image: "",
+    image: i11,
     },
     {
     id: 12,
     title: "realme c15 ",
     price: 'Rs 19,500',
     description: "Realme C15 Qualcomm Edition smartphone was launched on 28th October 2020. The phone comes with a 6.50-inch touchscreen display with a resolution of 720x1600 pixels and an aspect ratio of 20:9. Realme C15 Qualcomm Edition is powered by an octa-core Qualcomm Snapdragon 460 processor. It comes with 3GB of RAM.",
-    image: "",
+    image: i12,
     },
 ]
