d41065c16ea6f16a93a47911e620bdb9c467583a
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-04-21 08:51:11 +0545

    adding filter products

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import { SliderData } from "./components/Hero/SliderData";
 import Product from "./components/product/Product";
-import { ProductData } from "./components/product/ProductData";
-import Filter from "./components/product/Filter";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero slides={SliderData} />
-      <Filter
-      />
-      <Product items={ProductData} />
+      <Product />
     </div>
   );
 }
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,22 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Product.css'
+import { ProductData } from './ProductData'
 
-const Product = ({items}) => {
+
+const Product = () => {
+    const [filter, setFilter] = useState({
+        filter: 'All',
+        products: [...ProductData],
+    });
+    const [brand, setBrand] = useState([]);
+
+    useEffect(() => {
+        let brandArray = [];
+        ProductData.map((item) => brandArray.push(item.brand));
+        brandArray = new Set (brandArray);
+        setBrand(['All', ...brandArray]);
+    }, []);
+
+    const filterProducts = (event) => {
+        if (event.target.value === "All") {
+          setFilter({ filter: event.target.value, products: ProductData });
+        } else {
+          setFilter({
+            filter: event.target.value,
+            products: ProductData.filter(
+              (product) => product.brand.indexOf(event.target.value) >= 0
+            ),
+          });
+        }
+      };
+
+
     return (
+        <>
+        <div className="filter">
+        <h1>Products</h1>
+        <div className="filter-sort">
+          Order{" "}
+          <select>
+            <option>Latest</option>
+            <option>Lowest</option>
+            <option>Highest</option>
+          </select>
+          <div className="filer-size">
+            Filter{" "}
+            <select
+              name="brand"
+              value={filter.filter}
+              onChange={filterProducts}
+            >
+              {brand !== undefined
+                ? brand.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))
+                : ""}
+            </select>
+          </div>
+        </div>
+      </div>
             <div className="main_content">
-                {items.map((item) =>{
-                    return(
-                    <div className="card" key={item.id}>
-                        <img className="card_img" src={item.image} alt=""/>    
-                        <div className="card_header">
-                            <h2>{item.title}</h2>
-                            {/* <p>{item.description}</p> */}
-                            <p className="price">{item.price}</p>
-                            <div className="btn">Add to cart</div>
-                        </div>
-                    </div>
-                )})}
-            </div> 
+                {filter.products &&
+                filter.products.map((item) => {
+                return (
+                <div className="card" key={item.id}>
+                <img className="card_img" src={item.image} alt="" />
+                <div className="card_header">
+                  <h2>{item.title}</h2>
+                  {/* <p>{item.description}</p> */}
+                  <p className="price">{item.price}</p>
+                  <div className="btn">Add to cart</div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+            </> 
     )
 }
 
